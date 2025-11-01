6ee0a8989a3b550c1c9230ce9906fe15e3b78b8d
Akhil <akhilbhojne@gmail.com>
2025-11-01 13:14:51 +0530

    making some updates to urls file

--- events/urls.py
+++ events/urls.py
@@ -5,7 +5,7 @@ from .views import DirectInstallView
 
 urlpatterns = [
     path('events/', Events.as_view(), name='slack-events'),
-    path('commands/', SlashCommandView.as_view(), name='slack-commands'),
+    path('command/', SlashCommandView.as_view(), name='slack-command'),
     path('interact/', InteractionView.as_view(), name='slack-interact'),
     path('oauth_redirect/', OAuthRedirectView.as_view(), name='slack-oauth-redirect'),
     path('install/', DirectInstallView.as_view(), name='slack-direct-install'),
